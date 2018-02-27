698dab05d83e2c0730d60ba27338d680ef244c48
ukchong <31201153+ukchong@users.noreply.github.com>
2018-02-27 17:53:18 +1100

    Complete homework
    - done with exercise1
    - played around with settings a bit

--- week1/exercise1.py
+++ week1/exercise1.py
@@ -2,3 +2,8 @@
 
 TODO: write a python script that first prints "hello world!"
 """
+print("Hello world!")
+"""ARE YOU FINE WITH CAPS?
+It means string if contained between
+click the empty section to put red dots, do you have to type line by line by the way?
+what other functions other than print I can use"""
